a9c07ea739bf24a2713e0239c407af8849cb9b95
jdavenportgit <83671282+jdavenportgit@users.noreply.github.com>
2021-05-04 18:48:02 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -14,4 +14,8 @@ class minecraft {
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
+  service {'minecraft':
+    ensure => running,
+    enable => true,
+  }
 }
