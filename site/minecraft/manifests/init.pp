a7c603160f82a958a723e5a09c9604b0479034da
jdavenportgit <83671282+jdavenportgit@users.noreply.github.com>
2021-05-04 19:46:59 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,15 +1,15 @@
-class minecraft (
-  $url = 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+ass minecraft (
+  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
   $install_dir = '/opt/minecraft'
 ){
-  file {$install_dir:
+  file { $install_dir:
     ensure => directory,
   }
-  file {"${install_dir}/server.jar':
+  file { "${install_dir}/minecraft_server.jar":
     ensure => file,
     source => $url,
-    before => Service['minecraft'],
   }
+
   package {'java':
     ensure => present,
   }
@@ -17,16 +17,14 @@ class minecraft (
     ensure => file,
     content => 'eula=true'
   }
-  file {'/etc/systemd/system/minecraft.service':
+  file {â€˜/etc/systemd/system/minecraft.serviceâ€™:
     ensure => file,
-    content => epp('minecraft/minecraft.service',{
-    
-    install_dir => $install_dir,
-    })
+    content => epp(â€˜minecraft/minecraft.service.eppâ€™, {
+      install_dir => $install_dir,
+    }),
   }
-  service {'minecraft':
+  service { 'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java'],File["{install_dir}/eula.txt"],File['/etc/systmd/system/minecraft.service']],
   }
 }
